a51f8ef990632fcf25e4ac0fdccb6659671e362d
Daniel <daniel.torresg@udea.edu.co>
2021-10-12 18:59:46 -0500

    deleting login endpoinst

--- app/api/api.py
+++ app/api/api.py
@@ -1,8 +1,7 @@
 from fastapi import APIRouter
 
-from app.api.endpoints import dog, login, user
+from app.api.endpoints import dog, user
 
 api_router = APIRouter()
-api_router.include_router(login.router, tags=["login"])
 api_router.include_router(dog.router, prefix="/dogs", tags=["dog"])
 api_router.include_router(user.router, prefix="/users", tags=["user"])
